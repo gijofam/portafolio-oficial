3825094e5ed3550d4f9fcacd471f8d2c62165973
gilmar <gipajfm@gmail.com>
2022-09-15 17:17:22 -0500

    portafolio todas las secciones

---
+++ assets/script/main.js
@@ -0,0 +1,64 @@
+const puestoWork = document.getElementById('cargo')
+
+const maquinaEscribir = (str) => {
+ puestoWork.innerHTML = ''
+ let i=0;
+ let j = str.length;
+ let arrayCaracter = str.split('')
+ const escribir = setInterval( () => {
+    puestoWork.innerHTML += arrayCaracter[i];
+    i++;
+    if(i === arrayCaracter.length)
+     {
+       clearInterval(escribir)
+     }
+    // if(i === arrayCaracter.length)
+    //  {
+    //     puestoWork.innerHTML = str.substring(0,j);
+    //     j--;
+    //     if(j === 0){
+    //         i=0;
+    //         j = str.length;
+    //         puestoWork.innerHTML = '';
+    //     }
+    //  }else{
+    //     puestoWork.innerHTML += arrayCaracter[i];
+    //     i++;
+    //  }
+   
+ }, 300)
+}
+
+maquinaEscribir(' Desarrollador Web - Frontend')
+// puestoWork.innerHTML += 'Desarrollador Web - Frontend'
+
+const form = document.getElementById('form-contact')
+const correo = 'gipajfm@gmail.com'
+const url = `https://formsubmit.co/ajax/${correo}`
+form.addEventListener('submit', (e)=>{
+  e.preventDefault()
+  // console.log(e.currentTarget.elements);
+  const inputs = e.currentTarget.elements;
+  const dataForm = {
+    name: inputs.name.value,
+    email: inputs.correo.value,
+    message: inputs.description.value,
+  }
+  // console.log(dataForm)
+  
+// https://github.com/github/fetch
+  fetch(url, {
+    method: "POST",
+    headers: { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    },
+    body: JSON.stringify(dataForm)
+  })
+    
+    // .then(response => response.json())
+    // .then(data => console.log(data))
+    // .catch(error => console.log(error));
+    alert('tu mensaje en envio con exito')
+    e.currentTarget.reset()
+})
